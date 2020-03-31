f11a7b2e1db2ff8decf4451c2aa7586d1113c866
Automatic CI Releaser <ltrubov@chocolateplatform.com>
2020-03-30 20:36:58 -0700

    swift sample added: only banners

--- FreestarSwiftSample/FreestarSwiftSample/AppDelegate.swift
+++ FreestarSwiftSample/FreestarSwiftSample/AppDelegate.swift
@@ -7,13 +7,19 @@
 //
 
 import UIKit
+import FreestarAds
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    static let FREESTAR_API_KEY = "P8RIA3"
+    
     var window : UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Freestar.initWithAdUnitID(AppDelegate.FREESTAR_API_KEY)
         
         window = UIWindow()
         window?.frame = UIScreen.main.bounds
