f475fe7c69a08ccbcb55cec63552e223338b1a24
Dean Chang <dean.chang@freestar.io>
2022-04-19 16:17:46 -0700

    Update for unity multi-mediation.

--- FreestarSwiftSample/FreestarSwiftSample/AppDelegate.swift
+++ FreestarSwiftSample/FreestarSwiftSample/AppDelegate.swift
@@ -12,7 +12,7 @@ import FreestarAds
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
 //    static let FREESTAR_API_KEY = "ef05da0e-1c5f-4595-b835-74ecedf048dd"
-    static let FREESTAR_API_KEY = "1d10c713-cdc8-4d98-9747-1a0724904080"
+    static let FREESTAR_API_KEY = "43bf5367-bcad-4707-91f8-64dbb9086c60"
 //    static let FREESTAR_API_KEY = "XqjhRR"
     
     var window : UIWindow?
