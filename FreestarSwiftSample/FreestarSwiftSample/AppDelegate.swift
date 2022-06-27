c7426d1699293874f7d69406e351338337a2fa04
Carlos Alcala <carlos.alcala@freestar.io>
2022-06-27 17:57:45 -0400

    Yahoo App Delegate Testing

--- FreestarSwiftSample/FreestarSwiftSample/AppDelegate.swift
+++ FreestarSwiftSample/FreestarSwiftSample/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         Freestar.setLoggingEnabled(true)
-        Freestar.setTestModeEnabled(true)
+        Freestar.setTestModeEnabled(false)
         Freestar.setAdaptiveBannerEnabledIfAvailable(true)
 //        Freestar.setServingMode(.admobGam)
         Freestar.initWithAppKey(AppDelegate.FREESTAR_API_KEY)
