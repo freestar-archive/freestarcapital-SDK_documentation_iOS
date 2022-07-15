5f68503eb99793d0ec81cda865926fd6b76db806
Carlos Alcala <carlos.alcala@freestar.io>
2022-07-15 18:29:57 -0400

    Sample changes for Yahoo SDK test

--- FreestarSwiftSample/FreestarSwiftSample/AppDelegate.swift
+++ FreestarSwiftSample/FreestarSwiftSample/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //    static let FREESTAR_API_KEY = "ef05da0e-1c5f-4595-b835-74ecedf048dd"
 //    static let FREESTAR_API_KEY = "5fee93b8-d3d5-43ce-84a5-bffc90e81b5b"
       static let FREESTAR_API_KEY = "37f63777-6e63-42f2-89b7-4b67689c2493"
+//    static let FREESTAR_API_KEY = "1d10c713-cdc8-4d98-9747-1a0724904080"
 //    static let FREESTAR_API_KEY = "XqjhRR"
     
     var window : UIWindow?
@@ -21,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         Freestar.setLoggingEnabled(true)
-        Freestar.setTestModeEnabled(false)
+        Freestar.setTestModeEnabled(true)
         Freestar.setAdaptiveBannerEnabledIfAvailable(true)
 //        Freestar.setServingMode(.admobGam)
         Freestar.initWithAppKey(AppDelegate.FREESTAR_API_KEY)
