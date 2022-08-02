c8762b55bb36d37f49931efb9e74633b525fc391
Carlos Alcala <carlos.alcala@freestar.io>
2022-08-02 19:33:26 -0400

    Implemented Confiant PROPERTY_ID

--- FreestarSwiftSample/FreestarSwiftSample/AppDelegate.swift
+++ FreestarSwiftSample/FreestarSwiftSample/AppDelegate.swift
@@ -65,11 +65,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
             print("\(error)")
         }
+        //Confiant Initialize
+        Freestar.confiantInitialize(with: "Vw6Hmt4tR8gdJgdE_ZvL9fmbqdE")
         
         return true
     }
-
-    
 
 }
 
