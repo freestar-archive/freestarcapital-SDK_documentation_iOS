5ae2a7cc0a9ba45b6650c1faef88a4aaa17208db
Carlos Alcala <carlos.alcala@freestar.io>
2022-08-30 21:11:37 -0400

    Implemented ThumbnailViewController

---
+++ FreestarOBJCSample/FreestarOBJCSample/ThumbnailViewController.m
@@ -0,0 +1,25 @@
+//
+//  ThumbnailViewController.m
+//
+//  Created by Carlos Alcala on 8/30/22.
+//  Copyright © 2019. All rights reserved.
+//
+
+#import "ThumbnailViewController.h"
+#import "ViewController+FreestarAds.h"
+#import "PartnerSelectionTableViewController.h"
+#import <UIKit/UIKit.h>
+#import <OgurySdk/Ogury.h>
+#import <OguryAds/OguryAds.h>
+#import <OguryChoiceManager/OguryChoiceManager.h>
+
+
+@interface ThumbnailViewController: UIViewController
+
+@property (nonatomic, strong) OguryThumbnailAd *thumbnailAd;
+
+@end
+
+@implementation ThumbnailViewController
+
+@end
