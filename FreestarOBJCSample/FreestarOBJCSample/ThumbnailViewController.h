d9439c77ea8fdc440e479e1170dd8d504ba49b1a
Carlos Alcala <carlos.alcala@freestar.io>
2022-09-02 19:48:33 -0400

    Implemented ThumbnailViewController OBJCSample

---
+++ FreestarOBJCSample/FreestarOBJCSample/ThumbnailViewController.h
@@ -0,0 +1,12 @@
+//
+//  ThumbnailViewController.h
+//
+//  Created by Carlos Alcala on 9/02/22.
+//  Copyright © 2019. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThumbnailViewController : UIViewController
+
+@end
